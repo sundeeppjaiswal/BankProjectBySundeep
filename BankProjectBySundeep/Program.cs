94c15ac72584130e34db60b5a054e31da2733263
Sundeep Prakash Jaiswal <sundeeppjaiswal@hotmail.com>
2024-07-29 12:09:28 +0530

    Created the main menu for the bank
    Created the main menu for the bank

--- BankProjectBySundeep/Program.cs
+++ BankProjectBySundeep/Program.cs
@@ -20,8 +20,46 @@ if (userName != "")
 //check username and password
 if (userName == "system" && password == "manager")
 {
-    Console.WriteLine("TO DO: Main menu here");
+
+    //declare variable to store menu choice
+    int mainMenuChoice = -1;
+
+    do
+    {
+        //show main menu
+        System.Console.WriteLine("\n***Main menu***");
+        System.Console.WriteLine("1. Customers");
+        System.Console.WriteLine("2. Accounts");
+        System.Console.WriteLine("3. Funds Transfer");
+        System.Console.WriteLine("4. Funds Transfer Statement");
+        System.Console.WriteLine("5. Account Statement");
+        System.Console.WriteLine("0. Exit");
+
+        //accept menu choice from keyboard
+        System.Console.Write("Enter choice: ");
+        mainMenuChoice = int.Parse(Console.ReadLine());
+
+        //switch-case to check menu choice
+        switch (mainMenuChoice)
+        {
+            case 1: //TO DO: Display customers menu
+                break;
+            case 2: //TO DO: Display accounts menu
+                break;
+            case 3: //TO DO: Display funds transfer menu
+                break;
+            case 4: //TO DO: Display funds transfer statement menu
+                break;
+            case 5: //TO DO: Display account statement menu
+                break;
+        }
+    } while (mainMenuChoice != 0);
 }
+else
+{
+    System.Console.WriteLine("Invalid username or password");
+}
+
 
 //about to exit
 Console.WriteLine("Thank you! Visit again.");
--- BankProjectBySundeep/obj/Debug/net8.0/BankProjectBySundeep.AssemblyInfo.cs
+++ BankProjectBySundeep/obj/Debug/net8.0/BankProjectBySundeep.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankProjectBySundeep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adc659a8616aa8f9df69204f5515b07c5631c5a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f07e31f1a6159764cc502edaac73e995dbd41f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankProjectBySundeep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankProjectBySundeep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
