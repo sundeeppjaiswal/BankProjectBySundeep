8f07e31f1a6159764cc502edaac73e995dbd41f9
Sundeep Prakash Jaiswal <sundeeppjaiswal@hotmail.com>
2024-07-29 12:04:44 +0530

    Created a login form for the banking project
    Created a login form for the banking project

---
+++ BankProjectBySundeep/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//display title
+Console.WriteLine("************** Bank Project by Sundeep *****************");
+Console.WriteLine("**Login Page**");
+
+//declare variables to store username and password;
+string userName = null, password = null;
+
+//read userName from keyboard
+Console.Write("Username: ");
+userName = Console.ReadLine();
+
+//read password from keyboard only if username is entered
+if (userName != "")
+{
+    Console.Write("Password: ");
+    password = Console.ReadLine();
+}
+
+//check username and password
+if (userName == "system" && password == "manager")
+{
+    Console.WriteLine("TO DO: Main menu here");
+}
+
+//about to exit
+Console.WriteLine("Thank you! Visit again.");
+Console.ReadKey();
